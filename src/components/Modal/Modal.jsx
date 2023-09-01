4510e32a544c4863c07c2d9f2302305b3da60f5f
vladyslavbabenk0 <thevladman17@gmail.com>
2023-09-01 20:52:49 +0200

    commit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,52 +1,48 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
-class Modal extends Component {
-  static propTypes = {
-    toggleModal: PropTypes.func.isRequired,
-    largeImage: PropTypes.shape({
-      src: PropTypes.string.isRequired,
-    }),
-  };
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+const Modal = ({ toggleModal, largeImage }) => {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === 'Escape') {
+        toggleModal();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    window.addEventListener('keydown', handleKeyDown);
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      return this.props.toggleModal();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toggleModal]);
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
-      return this.props.toggleModal();
+      toggleModal();
     }
   };
 
-  render() {
-    const { handleBackdropClick } = this;
-    const {
-      largeImage: { src },
-    } = this.props;
+  const { src } = largeImage;
 
-    return createPortal(
-      <div className={styles.Overlay} onClick={handleBackdropClick}>
-        <div className={styles.Modal}>
-          <img src={src} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <div className={styles.Overlay} onClick={handleBackdropClick}>
+      <div className={styles.Modal}>
+        <img src={src} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
+
+Modal.propTypes = {
+  toggleModal: PropTypes.func.isRequired,
+  largeImage: PropTypes.shape({
+    src: PropTypes.string.isRequired,
+  }),
+};
 
 export default Modal;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,66 +1,56 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './Searchbar.module.css';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 
-class Searchbar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+const Searchbar = ({ onSubmit }) => {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearch(value);
   };
 
-  state = {
-    search: '',
-  };
-
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    
-    const { search } = this.state;
+
     if (search.trim() === '') {
       toast.error('Enter your search query');
       return;
     }
-    
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.reset();
+
+    onSubmit({ search });
+    reset();
   };
 
-  reset() {
-    this.setState({
-      search: '',
-    });
-  }
+  const reset = () => {
+    setSearch('');
+  };
 
-  render() {
-    const { search } = this.state;
-    const { handleChange, handleSubmit } = this;
-    return (
-      <header className={styles.Searchbar}>
-        <form className={styles.SearchForm} onSubmit={handleSubmit}>
-          <button type="submit" className={styles.SearchForm__button}>
-            <span className={styles.SearchForm__button__label}>Search</span>
-          </button>
+  return (
+    <header className={styles.Searchbar}>
+      <form className={styles.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={styles.SearchForm__button}>
+          <span className={styles.SearchForm__button__label}>Search</span>
+        </button>
 
-          <input
-            className={styles.SearchForm__input}
-            onChange={handleChange}
-            name="search"
-            value={search}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={styles.SearchForm__input}
+          onChange={handleChange}
+          value={search}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          name="search"
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default Searchbar;
